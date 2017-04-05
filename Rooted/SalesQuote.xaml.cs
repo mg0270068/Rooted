3e252c33763f2499b97fb511f4f2bbbe68790a16
MARK GRANT <mg0270068@otc.edu>
2017-04-05 04:05:53 -0500

    Added entry fields for the sales quote page.

--- Rooted/SalesQuote.xaml.cs
+++ Rooted/SalesQuote.xaml.cs
@@ -24,5 +24,10 @@ namespace Rooted
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
