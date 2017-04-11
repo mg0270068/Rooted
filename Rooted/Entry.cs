555a5cfbdaba123781cae12f98e5c924a013d89f
cb0784263 <cb0784263@DT036646>
2017-04-11 14:07:05 -0500

    created an output string for testing the class, and verified that it does in fact correctly hold the data

--- Rooted/Entry.cs
+++ Rooted/Entry.cs
@@ -12,6 +12,7 @@ namespace Rooted
         string firstName;
         string lastName;
         string address;
+        string phoneNumber;
         string city;
         string state;
         string zip;
@@ -19,13 +20,25 @@ namespace Rooted
         double tax;
         double finalSale;
 
-
-        //public entry class for access by other sections of the program
-        public Entry(string fname, string lname, string add, string ct, string st, string zp, double initsale, double tx, double fsale)
+        //first constructor; will pass this entry into the finalized entry
+        public Entry(string fname, string lname, string add, string phone, string ct, string st, string zp)
         {
             FirstName = fname;
             LastName = lname;
             Address = add;
+            PhoneNumber = phone;
+            City = ct;
+            State = st;
+            Zip = zp;
+        }
+
+        //overloaded constructor
+        public Entry(string fname, string lname, string add, string phone, string ct, string st, string zp, double initsale, double tx, double fsale)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Address = add;
+            PhoneNumber = phone;
             City = ct;
             State = st;
             Zip = zp;
@@ -37,7 +50,7 @@ namespace Rooted
         //just in case
         public override string ToString()
         {
-            return firstName + " " + lastName + ", " + "at " + address + " " + city + ", " + state + " " + zip + "\nInitial Sale: " + initialSale + "\nTax: " + tax + "\nFinal Sale: " + finalSale;
+            return firstName + " " + lastName + ", " + "at " + address + " " + city + ", " + state + " " + zip + "with phone number " + phoneNumber/*+ "\nInitial Sale: " + initialSale + "\nTax: " + tax + "\nFinal Sale: " + finalSale*/;
         }
 
         //TODO: figure out what data we're sending to QuickBooks, then implement this method to send variables where necessary
@@ -165,5 +178,19 @@ namespace Rooted
             }
         }
 
+        public string PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+
+            set
+            {
+                phoneNumber = value;
+            }
+        }
     }
 }
+
+//TODO: Copy this into a final submission form
